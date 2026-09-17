5a34324f0b63b7abf88e985f5aab4a7987d664a2
JunwonTomOh <junwon.oh@cern.ch>
2026-09-17 12:48:44 +0900

    Update 2016 trigger eff

---
+++ inc/higgsino/apply_trigeffs2016APV.hpp
@@ -0,0 +1,16 @@
+#ifndef H_APPLY_TRIGEFFS_2016APV
+#define H_APPLY_TRIGEFFS_2016APV
+
+#include "core/named_func.hpp"
+
+namespace Higfuncs {
+  extern const NamedFunc get_0l_trigeff2016APV;
+  extern const NamedFunc get_0l_trigeff2016APV_mettru;
+  extern const NamedFunc get_0l_fakemet_trigeff2016APV;
+  extern const NamedFunc get_1el_trigeff2016APV;
+  extern const NamedFunc get_1mu_trigeff2016APV;
+  extern const NamedFunc get_2el_trigeff2016APV;
+  extern const NamedFunc get_2mu_trigeff2016APV;
+}
+
+#endif
---
+++ inc/higgsino/apply_trigeffs2016postVFP.hpp
@@ -0,0 +1,16 @@
+#ifndef H_APPLY_TRIGEFFS_2016POSTVFP
+#define H_APPLY_TRIGEFFS_2016POSTVFP
+
+#include "core/named_func.hpp"
+
+namespace Higfuncs {
+  extern const NamedFunc get_0l_trigeff2016postVFP;
+  extern const NamedFunc get_0l_trigeff2016postVFP_mettru;
+  extern const NamedFunc get_0l_fakemet_trigeff2016postVFP;
+  extern const NamedFunc get_1el_trigeff2016postVFP;
+  extern const NamedFunc get_1mu_trigeff2016postVFP;
+  extern const NamedFunc get_2el_trigeff2016postVFP;
+  extern const NamedFunc get_2mu_trigeff2016postVFP;
+}
+
+#endif
